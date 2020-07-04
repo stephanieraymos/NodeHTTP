bc4236457628e73739b9c91deb5c96e8e165ff5d
Stephanie Raymos <stephanieraymos@gmail.com>
2020-07-03 17:51:27 -0700

    Added comments

--- server.js
+++ server.js
@@ -3,13 +3,18 @@ const http = require('http');
 const hostname = 'localhost';
 const port = 3000;
 
+//You don't create the http request, it is onlly received
 const server = http.createServer((req, res) => {
     console.log(req.headers);
     res.statusCode = 200;
+    //Anytime html is send in the body; the header should be set just like this:
     res.setHeader('Content-Type', 'text/html');
+    //When body is short; you can simply include it in the res.end instead of creating a res.write
     res.end('<html><body><h1>Hello, World!</h1></body></html>');
+    //response is now completed and the info will be sent back to the client
 });
 
 server.listen(port, hostname, () => {
+    //executed when the server starts:
     console.log(`Server running at http://${hostname}:${port}/`);
 });
